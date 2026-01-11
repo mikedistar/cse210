d1be32b50e6b0337a508e5f1cf0397938adffd99
Michael Starling <sta24049@byui.edu>
2026-01-10 23:12:10 -0700

    Follwed the assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percent = Console.ReadLine();
+
+        float percentage = float.Parse(percent);
+        string grade = "A";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage < 90 && percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage < 80 && percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage < 70 && percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"You got a {grade}.");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time.");
+        }
     }
 }
