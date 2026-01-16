06e2f67863263cf02f6ffdd8d43c5b38df7ae9a7
Michael Starling <sta24049@byui.edu>
2026-01-15 22:19:41 -0700

    Made a number list that adds numbers together, finds the average, and the largest number.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1;
+        int sum = 0;
+        int largest_number = 0;
+        int total = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter a whole number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+                sum += number;
+                total += 1;
+                if (number > largest_number)
+                {
+                    largest_number = number;
+                }
+            }
+        }
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {sum/total}");
+        Console.WriteLine($"The largest number is {largest_number}");
+
     }
 }
