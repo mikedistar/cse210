8b08c259297a5469d04dc29a8d814aaf8784caed
Michael Starling <sta24049@byui.edu>
2026-01-15 23:13:10 -0700

    The program does everything it needs to, except I didn't use the out parameter.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Enter an integer: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int PromtUserBirthYear()
+        {
+            Console.Write("Which year were you born? ");
+            int year = int.Parse(Console.ReadLine());
+            return year;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squared_number = number * number;
+            return squared_number;
+        }
+
+        static int DisplayResult(string name, int squared_number, int year)
+        {
+            int current_year = 2026;
+            Console.WriteLine($"{name}, the square of your number is {squared_number}");
+            Console.WriteLine($"{name}, you will turn {current_year - year}");
+
+            return 3;
+        }
+
+        int year;
+        DisplayMessage();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared_number = SquareNumber(number);
+        year = PromtUserBirthYear();
+        DisplayResult(name, squared_number, year);
+        
+
     }
 }
