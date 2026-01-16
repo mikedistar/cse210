0913dac18dfa0fccf84edf65fab86d8d4f13388c
Michael Starling <sta24049@byui.edu>
2026-01-15 21:57:25 -0700

    I made a number guessing game with this program.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 11);
+        int number = 0;
+        while (number != magic_number)
+        {
+            Console.Write("Guess my magic number (it's a whole number): ");
+            string guess = Console.ReadLine();
+            number = int.Parse(guess);
+
+            if (number > magic_number)
+            {
+                Console.WriteLine("Guess lower.");
+            }
+            else if (magic_number > number)
+            {
+                Console.WriteLine("Guess higher.");
+            }
+            else
+            {
+                Console.WriteLine($"You're right! The number is {magic_number}!");
+            }
+        }
+
     }
 }
