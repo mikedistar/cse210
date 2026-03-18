5d4f42171b0ea47cc37297258f2f463060d28a83
Michael Starling <sta24049@byui.edu>
2026-03-17 18:14:28 -0600

    Made the scripture memorization program with the ability to select random scriptures.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,15 +5,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Scripture> scriptures = new List<Scripture>();
+        Random random = new Random();
 
-        string _userInput;
+        List<Scripture> scriptures = new List<Scripture>(); // Holds the list of scriptures
+
+        // Different references
+
+        Reference moroni = new Reference("Moroni", 10, 4, 5);
+        Reference alma = new Reference("Alma", 7, 11);
+        Reference nephi = new Reference("1 Nephi", 3, 7);
+        Reference helaman = new Reference("Helaman", 5, 12);
+        Reference mosiah = new Reference("Mosiah", 2, 17);
+
+        // Different scriptures with text and reference
+
+        Scripture moroni10 = new Scripture(moroni, "And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, if these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy Ghost. And by the power of the Holy Ghost ye may know the truth of all things.");
+        Scripture alma7 = new Scripture(alma, "And he shall go forth, suffering pains and afflictions and temptations of every kind; and this that the word might be fulfilled which saith he will take upon him the pains and the sicknesses of his people.");
+        Scripture nephi3 = new Scripture(nephi, "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.");
+        Scripture helaman5 = new Scripture(helaman, "And now, my sons, remember, remember that it is upon the rock of our Redeemer, who is Christ, the Son of God, that ye must build your foundation; that when the devil shall send forth his mighty winds, yea, his shafts in the whirlwind, yea, when all his hail and his mighty storm shall beat upon you, it shall have no power over you to drag you down to the gulf of misery and endless wo, because of the rock upon which ye are built, which is a sure foundation, a foundation whereon if men build they cannot fall.");
+        Scripture mosiah2 = new Scripture(mosiah, "17 And behold, I tell you these things that ye may learn wisdom; that ye may learn that when ye are in the service of your fellow beings ye are only in the service of your God.");
+
+        // Adding scriptures to scritpures list
+
+        scriptures.Add(moroni10);
+        scriptures.Add(alma7);
+        scriptures.Add(nephi3);
+        scriptures.Add(helaman5);
+        scriptures.Add(mosiah2);
+
+        int number = random.Next(0,4);
+
+        Scripture chosenScripture = scriptures[number];
+
+
+        string _userInput = "";
+
+        chosenScripture.PrintReference();
+        Console.WriteLine($"{chosenScripture.GetText()}");
+
+        while (_userInput != "quit" & chosenScripture.CheckIfAllIsHidden() != true)
+        {
+        Console.WriteLine();
         Console.WriteLine("Press ENTER or type 'quit' to exit.");
         _userInput = Console.ReadLine();
+
+            if (_userInput != "quit" & chosenScripture.CheckIfAllIsHidden() != true)
+            {
+                Console.Clear();
+                chosenScripture.PrintReference();
+                chosenScripture.HideWords();
+                chosenScripture.CheckIfAllIsHidden();
+                Console.WriteLine();
+            }
+        }
     }
 
-    public Scripture PickRandomScripture()
-    {
-
-    }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -4,8 +4,9 @@ class Scripture
     private Reference _reference;
 
     private string _text;
-
-    private List<Word> _words;
+    Random _random = new Random();
+    private List<Word> _words = new List<Word>();
+    int _lengthOfList = 0;
 
 
     public Scripture(Reference reference, string text)
@@ -16,10 +17,15 @@ class Scripture
         {
             Word word1 = new Word(word);
             _words.Add(word1);
+
+            _lengthOfList ++;
         }
     }
 
-
+    public void PrintReference()
+    {
+        Console.WriteLine($"{_reference.GetReference()}");
+    }
     public string GetText()
     {
         return _text;
@@ -31,14 +37,45 @@ class Scripture
     public void HideWords()
     {
 
+        // Picks random words to hide in scripture
 
+       Word _chosenWord1 = _words[_random.Next(0, _lengthOfList)];
+       Word _chosenWord2 = _words[_random.Next(0, _lengthOfList)];
+       Word _chosenWord3 = _words[_random.Next(0, _lengthOfList)];
 
-        Console.Clear();
+       // Those words hide themselves
+
+       _chosenWord1.HideWord();
+       _chosenWord2.HideWord();
+       _chosenWord3.HideWord();
+
+       foreach(Word _word in _words)
+        {
+           Console.Write($" {_word.RenderText()}");
+        }
 
 
     }
-    public void CheckIfAllIsHidden()
+    public bool CheckIfAllIsHidden()
     {
+        int _numberOfShownWords = 0;
+        foreach (Word _word in _words)
+        {
+            if (_word.GetIsHidden() == false)
+            {
+                _numberOfShownWords ++;
+            }
+        }
 
+    if(_numberOfShownWords > 0)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            
+            }
     }
 }
